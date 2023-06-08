22005e514c4c1ddf980b4062d8a9603cc869724c
Lourens Walters <lourens.walters@informa.com>
2023-06-08 11:00:15 +0100

    Added Twitter processing

--- ice_breaker.py
+++ ice_breaker.py
@@ -4,6 +4,7 @@ from langchain.chains import LLMChain
 import os
 
 from agents.linkedin_lookup_agent import lookup as linkedin_lookup_agent
+from agents.twitter_lookup_agent import lookup as twitter_lookup_agent
 from third_parties.linkedin import scrape_linkedin_profile
 from third_parties.twitter import scrape_user_tweets
 from pathlib import Path
@@ -20,28 +21,31 @@ TWITTER_API_SECRET = os.environ.get("TWITTER_API_SECRET")
 TWITTER_ACCESS_TOKEN = os.environ.get("TWITTER_ACCESS_TOKEN")
 TWITTER_ACCESS_SECRET = os.environ.get("TWITTER_ACCESS_SECRET")
 
+name = "Harrison Chase"
+
 if __name__ == "__main__":
     print("Hello LangChain!")
 
-    # linkedin_profile_url = linkedin_lookup_agent(name="Eden Marco Udemy")
-    #
-    # summary_template = """
-    #      given the Linkedin information {information} about a person from I want you to create:
-    #      1. a short summary
-    #      2. two interesting facts about them
-    #  """
-    #
-    # summary_prompt_template = PromptTemplate(
-    #     input_variables=["information"], template=summary_template
-    # )
-    #
-    # llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
-    #
-    # chain = LLMChain(llm=llm, prompt=summary_prompt_template)
-    #
-    # linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
-    #
-    # print(chain.run(information=linkedin_data))
+    linkedin_profile_url = linkedin_lookup_agent(name=name)
+    linkedin_data = scrape_linkedin_profile(linkedin_profile_url=linkedin_profile_url)
 
-    tweets = scrape_user_tweets(username="@elonmusk", num_tweets=100)
-    print (tweets)
+    twitter_username = twitter_lookup_agent(name=name)
+    tweets = scrape_user_tweets(username=twitter_username, num_tweets=5)
+
+    summary_template = """
+         given the Linkedin {linkedin_information} and twitter information {twitter_information} about a person I want you to create:
+         1. a short summary
+         2. two interesting facts about them
+     """
+
+    summary_prompt_template = PromptTemplate(
+        input_variables=["linkedin_information", "twitter_information"], template=summary_template
+    )
+
+    llm = ChatOpenAI(temperature=0, model_name="gpt-3.5-turbo")
+
+    chain = LLMChain(llm=llm, prompt=summary_prompt_template)
+
+    print(chain.run(linkedin_information=linkedin_data, twitter_information=tweets))
+
+    print(tweets)
